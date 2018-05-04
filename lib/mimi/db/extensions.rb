1dba2e12b3a1d67c1d440902a64224c16d0cf0d6
Alex Kukushkin <alex.kukushkin@strongkeep.com>
2018-05-04 13:58:54 +0200

    Disabled extensions for primary/foreign keys

--- lib/mimi/db/extensions.rb
+++ lib/mimi/db/extensions.rb
@@ -2,29 +2,13 @@ module Mimi
   module DB
     module Extensions
       def self.start
-        install_primary_keys!
-        install_bigint_foreign_keys!
-      end
-
-      def self.install_primary_keys!
-        ca = ActiveRecord::ConnectionAdapters
-        opts = Mimi::DB.module_options
-
-        if ca.const_defined?(:CockroachDBAdapter) && opts[:db_primary_key_cockroachdb]
-          ca::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:primary_key] = opts[:db_primary_key_cockroachdb]
-        end
-
-        if ca.const_defined?(:PostgreSQLAdapter) && opts[:db_primary_key_postgresql]
-          ca::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:primary_key] = opts[:db_primary_key_postgresql]
-        end
-
-        if ca.const_defined?(:AbstractMysqlAdapter) && opts[:db_primary_key_mysql]
-          ca::AbstractMysqlAdapter::NATIVE_DATABASE_TYPES[:primary_key] = opts[:db_primary_key_mysql]
-        end
+        # FIXME: refactor DSL for primary/foreign keys
+        # install_primary_keys!
+        # install_bigint_foreign_keys!
       end
 
       def self.install_bigint_foreign_keys!
-        ActiveRecord::Base.send(:include, Mimi::DB::ForeignKey)
+        # ActiveRecord::Base.send(:include, Mimi::DB::ForeignKey)
       end
     end # module Extensions
   end # module DB
