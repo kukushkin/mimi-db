dd4a5659cc164557fa01d820865fd3f4bbd03eaf
Alex Kukushkin <alex@kukushk.in>
2018-06-27 17:27:22 +0200

    Implemented indexes inspection for CockroachDB

--- lib/mimi/db/extensions/sequel-cockroachdb.rb
+++ lib/mimi/db/extensions/sequel-cockroachdb.rb
@@ -23,6 +23,32 @@ module Sequel
         # 100000 # mimics Postgres v10
       end
 
+      # Retrieves indexes for the given table
+      #
+      # NOTE: Apparently CockroachDB is not fully compatible with Postgres or Sequel's
+      # Postgres adapter, and it can't correctly figure out indexes and their properties.
+      # As a workaround, a specific #indexes() method is implemented here, which executes
+      # `SHOW INDEXES FROM ...` and parses the results.
+      #
+      # @param table_name [String,Symbol]
+      # @return [Hash] index_name => index_properties
+      #
+      def indexes(table_name)
+        idxs = {}
+        results = fetch('show indexes from ' + table_name.to_s).all
+        results.each do |idx_entry|
+          idx_name = idx_entry[:Name].to_sym
+          next if idx_name == :primary # ignore primary index
+          idxs[idx_name] ||= { name: idx_name.to_s }
+          idx = idxs[idx_name]
+          idx[:unique] = idx_entry[:Unique]
+          idx[:deferrable] = false
+          idx[:columns] ||= []
+          idx[:columns] << idx_entry[:Column].to_sym unless idx_entry[:Implicit]
+        end
+        idxs
+      end
+
       private
 
       def dataset_class_default
--- spec/mimi/db_spec.rb
+++ spec/mimi/db_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
-describe Mimi::Db do
+describe Mimi::DB do
   it 'has a version number' do
-    expect(Mimi::Db::VERSION).not_to be nil
+    expect(Mimi::DB::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  it 'has meaningful specs'
 end
